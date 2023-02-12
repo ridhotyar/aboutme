dbe20fc54db52fdca864a98fb6e56d327fadbdce
Ridho Tiyar <ridhotiyar@192.168.1.4>
2023-02-13 00:23:45 +0700

    Finishing Styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,37 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 
 // Moblie Indeed End //
 
+// HEADER star //
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+//Header end//
+
+//SCROLL REVEAL start//
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+
+//Scroll reveal end//
