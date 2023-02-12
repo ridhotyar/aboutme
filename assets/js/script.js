bf60cfc4cb1bac15ec9e9a5a42e76328ac010ca9
Ridho Tiyar <ridhotiyar@192.168.1.4>
2023-02-12 23:31:42 +0700

    add js and responsive 920px

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Moblie Indeed Start //
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// Moblie Indeed End //
+
